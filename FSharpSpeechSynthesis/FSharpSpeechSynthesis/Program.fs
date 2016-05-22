1fc758975987372393864b7341da9c58080f5b5d
Jesse Glover <silverdrakeproductions@gmail.com>
2016-05-22 17:06:46 -0400

    Removed the exception handling and added a string parameter to the voice function.

--- FSharpSpeechSynthesis/FSharpSpeechSynthesis/Program.fs
+++ FSharpSpeechSynthesis/FSharpSpeechSynthesis/Program.fs
@@ -1,26 +1,15 @@
 ﻿open System
 open System.Speech.Synthesis
 
-// reads the user's input
-let userInput() = Console.ReadLine()
-
-
-let voice = 
+let voice (sentence : string) = 
     // Speech Synthesizer initialization with the F# version of the using statement from C#
     use speech = new SpeechSynthesizer()
-    // 
-    try
     // Implicitly setting the voice to be the simplified Chinese voice made by Microsoft
     speech.SelectVoice("Microsoft Huihui Desktop")
     // seting the speech to derive from the user's imput
-    speech.Speak(userInput())
-    with
-    // A little bit of exception handling
-    | :? System.ArgumentException as ex -> ()
-    | ex -> printf "%s" ex.Message
-    
+    speech.Speak(sentence)
 
-// entry point for the program which will actually call the functions made above.
-let speechSynth = voice
+// entry point for the program which will actually call the voice function and read what the user Inputs
+let speak = voice(Console.ReadLine())
     
     
