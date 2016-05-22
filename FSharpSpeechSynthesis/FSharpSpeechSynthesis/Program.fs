3c686c909959d89eafabe7a91e4c76623b7cb7e0
Jesse Glover <silverdrakeproductions@gmail.com>
2016-05-22 03:39:13 -0400

    Entire tutorial project added to file.

---
+++ FSharpSpeechSynthesis/FSharpSpeechSynthesis/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+open System.Speech.Synthesis
+
+// reads the user's input
+let userInput() = Console.ReadLine()
+
+
+let voice = 
+    // Speech Synthesizer initialization with the F# version of the using statement from C#
+    use speech = new SpeechSynthesizer()
+    // 
+    try
+    // Implicitly setting the voice to be the simplified Chinese voice made by Microsoft
+    speech.SelectVoice("Microsoft Huihui Desktop")
+    // seting the speech to derive from the user's imput
+    speech.Speak(userInput())
+    with
+    // A little bit of exception handling
+    | :? System.ArgumentException as ex -> ()
+    | ex -> printf "%s" ex.Message
+    
+
+// entry point for the program which will actually call the functions made above.
+let speechSynth = voice
+    
+    
